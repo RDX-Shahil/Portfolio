add3161cd74ea080d923318deaf17b6f191b8575
RDX-Shahil <shahilahamad04@gmail.com>
2024-04-22 21:59:44 +0545

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
-    }
+  // Select all elements that match the given selector
+  const elements = document.querySelectorAll(elementSelector);
+  // Check if there are elements matching the selector and if the requested instance exists
+  if (elements.length > instance) {
+    // Scroll to the specified instance of the element
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
 }
 
 const link1 = document.getElementById("link1");
@@ -15,28 +15,27 @@ const link4 = document.getElementById("link4");
 const link5 = document.getElementById("link5");
 const link6 = document.getElementById("link6");
 
-
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
+link1.addEventListener("click", () => {
+  scrollToElement(".header");
 });
 
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
+link2.addEventListener("click", () => {
+  // Scroll to the second element with "header" class
+  scrollToElement(".header", 1);
 });
 
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
+link3.addEventListener("click", () => {
+  scrollToElement(".column");
 });
 
-link4.addEventListener('click', () => {
-    scrollToElement('.column');
+link4.addEventListener("click", () => {
+  scrollToElement(".column");
 });
 
-link5.addEventListener('click', () => {
-    scrollToElement('.column');
+link5.addEventListener("click", () => {
+  scrollToElement(".column");
 });
 
-link6.addEventListener('click', () => {
-    scrollToElement('.header',1);
+link6.addEventListener("click", () => {
+  scrollToElement(".header", 1);
 });
